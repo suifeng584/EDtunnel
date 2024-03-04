a5959af250b884d97f20de8d653e4b03b78b321d
suifeng584 <30490784+suifeng584@users.noreply.github.com>
2024-03-04 10:14:54 +0800

    Update _worker.js
    modify uuid

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '2c3cccc4-f033-4dd7-ac33-55a734ed70b7';
 
 const proxyIPs = ['cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'workers.bestip.one'];
 
